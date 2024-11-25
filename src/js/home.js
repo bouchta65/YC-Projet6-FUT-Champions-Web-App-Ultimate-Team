ce037aa890ad61c3e8bbf559dc97f9438d90bb0e
bouchta65 <bouchtamohamed01@gmail.com>
2024-11-25 16:50:54 +0100

    remove navbar links from the navbar

--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,11 @@
 const burgerIcon = document.getElementById("burger-icon");
 const navbar = document.querySelector(".navbar");
+const closeNavbar = document.getElementById("close-navbar");
 
 burgerIcon.addEventListener("click", () => {
-  navbar.classList.toggle("nav-active");
+  navbar.classList.add("nav-active");
 });
-console.log("hh")
+
+closeNavbar.addEventListener("click", () => {
+  navbar.classList.remove("nav-active");
+});
