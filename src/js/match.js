ad0dcb690519c1cbf04ee3a3991c6acd06bf6c01
bouchta65 <bouchtamohamed01@gmail.com>
2024-11-30 21:32:34 +0100

    Refactor player management:  fixed resetData UI update, enhanced playersPosition reinitialization, improved player selection validation, and optimized localStorage synchronization.

--- src/js/match.js
+++ src/js/match.js
@@ -1,52 +1,70 @@
+// Les Variables : 
+let filtredPlayers = []
+let dataPlus = ''
+
+const playerCard = document.querySelector('.player-cards')
+const plusCard = document.querySelector('.plus-cards')
+const playerPopup = document.querySelector('.player-popup')
+const selectedPosition =  document.getElementById("Position-filter")
+const positionSelect = document.getElementById("Position-filter");
+
+const selectedPlayers = JSON.parse(localStorage.getItem('selectedPlayers')) || [];
 const playersData = JSON.parse(localStorage.getItem("players")); 
 
-let table = "";
-function addPlayer(){
-    for(let i = 0 ; i<24 && i<playersData.length;i++){
-        table = 
-        `
 
+document.addEventListener('DOMContentLoaded', () => { 
+    replacePlusToPlayer()   
+    displayPlayerSelected()
+    playersPosition()
+    displayPlayersList()
+    resetData()
+});
+
+// Function to display players in the popup
+function displayerPlayerPopup(position){  
+    let table = ""
+    filtredPlayers = []
+    for(let i = 0 ; i<24 && i<playersData.length;i++){
+        if (playersData[i].position === position) {
+            filtredPlayers.push(playersData[i]);
+        }
+    }
+    for(let i = 0 ; i<24 && i<filtredPlayers.length;i++){
+
+        table += 
+        `
      <div class="player-card" data-index="${i}">
             <div class="card-header">
-                <img class="player-photo" src=${playersData[i].photo} alt="Lionel Messi">
+                <img class="player-photo" src=${filtredPlayers[i].photo} alt="Lionel Messi">
             </div>
             <div class="card-body">
-                <h4 class="player-name"> ${playersData[i].name}</h4>
+                <h4 class="player-name"> ${filtredPlayers[i].name}</h4>
                <div class="player-info2">
-                <p class="player-position">${playersData[i].position}</p>
-                <img class="club-logo" src="https://cdn.sofifa.net/meta/team/239235/120.png" alt="${playersData[i].logo}">
+                <p class="player-position">${filtredPlayers[i].position}</p>
+                <img class="club-logo" src=${filtredPlayers[i].logo} >
                </div>
             </div>
             <div class="card-footer">
-                
             </div>
             <div class="stats">
                 <div class="stat">
-                    <span>Rating:</span> <strong>${playersData[i].rating}</strong>
+                    <span>Rating:</span> <strong>${filtredPlayers[i].rating}</strong>
                 </div>
                 <div class="stat">
-                    <span>Pace:</span> <strong>${playersData[i].pace}</strong>
+                    <span>Pace:</span> <strong>${filtredPlayers[i].pace}</strong>
                 </div>
                 <div class="stat">
-                    <span>Shooting:</span> <strong>${playersData[i].shooting}</strong>
+                    <span>Shooting:</span> <strong>${filtredPlayers[i].shooting}</strong>
                 </div>
             </div>
         </div>
         `
-        document.querySelector('.player-cards').innerHTML +=table
-
-   
-    
-        
-    }
-
+        playerCard.innerHTML =table
+    }  
 }
 
-
-
-
-let dataPlus = ''
-function displayPlayers(){
+// Function to display players in the liste
+function displayPlayersList(){
     for (let i = 0; i < 24; i++) {
         dataPlus += `
          <div class="player-small-card" data-index="${i}">
@@ -60,101 +78,156 @@ function displayPlayers(){
         </div>
     </div>
 </div>
-
         `;
     }
-    document.querySelector('.plus-cards').innerHTML = dataPlus;
-}
-displayPlayers();
-
-
-
-
-
-function displayPopupSecond(){
-    document.querySelectorAll('.add-player-box').forEach((box) => {
-        box.addEventListener('click', () => {
-            playerPopup.classList.toggle("block");
-        });
-    })
+    plusCard.innerHTML = dataPlus;
 }
 
-
+// Function to change the position 
 function playersPosition(){
-    const positionSelect = document.getElementById("Position-filter");
     if(positionSelect.value === 'position2'){
-
-    document.querySelector('.players-position').innerHTML = 
+    document.querySelector('.plus-position').innerHTML = 
     `
-                <div class="add-player-box" data-index="0" style="top: 45%; left: 5%;"> <span class="plus-icon">+</span> </div>
+                <div class="add-player-box gk" data-index="0" style="top: 45%; left: 5%;"> <span class="plus-icon">+</span> </div>
               
-                  <div class="add-player-box" data-index="1" style="top: 10%; left: 25%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="2" style="top: 30%; left: 18%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="3" style="top: 60%; left: 18%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="4" style="top: 80%; left: 25%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box lb" data-index="1" style="top: 10%; left: 25%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cb" data-index="2" style="top: 30%; left: 18%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cb" data-index="3" style="top: 60%; left: 18%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box rb" data-index="4" style="top: 80%; left: 25%;"> <span class="plus-icon">+</span> </div>
               
-                  <div class="add-player-box" data-index="5" style="top: 10%; left: 55%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="6" style="top: 30%; left: 45%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="7" style="top: 60%; left: 45%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="8" style="top: 80%; left: 55%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box lw" data-index="5" style="top: 10%; left: 55%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cm" data-index="6" style="top: 30%; left: 45%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cm" data-index="7" style="top: 60%; left: 45%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box rw" data-index="8" style="top: 80%; left: 55%;"> <span class="plus-icon">+</span> </div>
               
-                  <div class="add-player-box" data-index="9" style="top: 30%; left: 78%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="10" style="top: 60%; left: 78%;"> <span class="plus-icon">+</span> </div>
-              
+                  <div class="add-player-box st" data-index="9" style="top: 30%; left: 78%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box st" data-index="10" style="top: 60%; left: 78%;"> <span class="plus-icon">+</span> </div>
                
     `
     }else{
-        document.querySelector('.players-position').innerHTML = 
+        document.querySelector('.plus-position').innerHTML = 
         `
-                <div class="add-player-box" data-index="0" style="top: 45%; left: 5%;"> <span class="plus-icon">+</span> </div>
+                <div class="add-player-box gk"  data-index="0" style="top: 45%; left: 5%;"> <span class="plus-icon">+</span> </div>
               
-                  <div class="add-player-box" data-index="1" style="top: 10%; left: 25%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="2" style="top: 30%; left: 18%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="3" style="top: 60%; left: 18%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="4" style="top: 80%; left: 25%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box lb" data-index="1" style="top: 10%; left: 25%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cb" data-index="2" style="top: 30%; left: 18%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cb" data-index="3" style="top: 60%; left: 18%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box rb" data-index="4" style="top: 80%; left: 25%;"> <span class="plus-icon">+</span> </div>
               
-                  <div class="add-player-box" data-index="5" style="top: 10%; left: 45%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="6" style="top: 45%; left: 45%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="7" style="top: 80%; left: 45%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cm" data-index="5" style="top: 10%; left: 45%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cm" data-index="6" style="top: 45%; left: 45%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box cm" data-index="7" style="top: 80%; left: 45%;"> <span class="plus-icon">+</span> </div>
               
-                  <div class="add-player-box" data-index="8" style="top: 10%; left: 70%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="9" style="top: 45%; left: 80%;"> <span class="plus-icon">+</span> </div>
-                  <div class="add-player-box" data-index="10" style="top: 80%; left: 70%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box lw" data-index="8" style="top: 10%; left: 70%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box st" data-index="9" style="top: 45%; left: 80%;"> <span class="plus-icon">+</span> </div>
+                  <div class="add-player-box rw" data-index="10" style="top: 80%; left: 70%;"> <span class="plus-icon">+</span> </div>
         `
     }
-    displayPopupSecond()
-    
+    resetData()
+    displayPlayerSelected()
+    replacePlusToPlayer() 
+   
+    selectedPosition.addEventListener("change", playersPosition);
 }
 
-const playerPopup = document.querySelector('.player-popup')
-document.getElementById("Position-filter").addEventListener("change", playersPosition);
-playersPosition();
-addPlayer()
-displayPopupSecond()
-
+//sclose the popup if after the click in colde button
 document.querySelector('.close-popup-btn').addEventListener('click',()=>{
     playerPopup.classList.toggle("block")
 })
 
+//
+function displayPlayerSelected() {
+    const addplayer = document.querySelectorAll('.add-player-box')
 
-playerPopup.addEventListener('click',()=>{
-    playerPopup.classList.toggle("block") 
-})
+    addplayer.forEach((box) => {
+        box.addEventListener('click', () => {
+           const boxSelected =  document.querySelector(`.add-player-box[data-index="${box.dataset.index}"]`)
+            playerPopup.classList.toggle("block");
+            
+                if (box.classList.contains('gk') ) {
+                    displayerPlayerPopup("GK");  
+                }else if(box.classList.contains('lb')){
+                    displayerPlayerPopup("LB"); 
+                }else if(box.classList.contains('cb')){
+                    displayerPlayerPopup("CB"); 
+                }else if(box.classList.contains('rb')){
+                    displayerPlayerPopup("RB"); 
+                }else if(box.classList.contains('cm')){
+                    displayerPlayerPopup("CM"); 
+                }else if(box.classList.contains('lw')){
+                    displayerPlayerPopup("LW"); 
+                }else if(box.classList.contains('st')){
+                    displayerPlayerPopup("ST"); 
+                }else if(box.classList.contains('rw')){
+                    displayerPlayerPopup("RW"); 
+                }
+                        
+                document.querySelectorAll('.player-card').forEach((card) => {
+                    card.addEventListener('click', () => {
+                        const playerIndex = card.getAttribute('data-index'); 
+                        const playerSlectedData = filtredPlayers[playerIndex];
 
+                        if (playerSlectedData && boxSelected) {
+                            let playerAlreadyExists = false;
+                            for (let i = 0; i < selectedPlayers.length; i++) {
+                                const existingPlayer = selectedPlayers[i];
+                                if (existingPlayer.player.name === playerSlectedData.name) {
+                                    playerAlreadyExists = true; 
+                                    alert("Le joueur est déjà ajouté dans une autre position.");
+                                    break;
+                                }
+                            }
 
-document.querySelector('.popup-content').addEventListener('click', (e) => {
-     e.stopPropagation(); 
-})
+                            if (playerAlreadyExists) {
+                                return;
+                            }
+                          
+                            for (let i = 0; i < selectedPlayers.length; i++) {
+                                if (selectedPlayers[i].boxIndex === boxSelected.dataset.index) {
+                                    selectedPlayers.splice(i, 1);
+                                    break;
+                                }
+                            }
 
+                            selectedPlayers.push({
+                                boxIndex: boxSelected.dataset.index, 
+                                player: playerSlectedData           
+                            });
 
-document.querySelectorAll('.add-player-box').forEach((box) => {box.addEventListener('click',()=>{
-    playerPopup.classList.toggle("block")
-   
-})
-})
-
-
-
-
-
+                            replacePlusToPlayer()
+                            localStorage.setItem("selectedPlayers", JSON.stringify(selectedPlayers));
+                        }
+                        playerPopup.classList.remove('block'); 
+                    });
+                });
+         
+        });
+    });
+}
+ 
+// clear the data after the click in reset button
+function resetData() {
+    document.querySelector('#resetbutton').addEventListener('click', () => {
+        localStorage.removeItem('selectedPlayers');
+        selectedPlayers.length = 0;
+        playersPosition();
+    });
+}
+ 
+// display the player selected in the terain
+function replacePlusToPlayer(){
+        for (let i = 0; i < selectedPlayers.length; i++) {
+            const { boxIndex, player } = selectedPlayers[i];
+            const box = document.querySelector(`.add-player-box[data-index="${boxIndex}"]`);
+          
+            if (box) {
+                box.classList.add("bgcolor");
+                box.innerHTML = `
+                    <div class="player2-image"><img src="${player.photo}" ></div>
+                    <span class="player2-rating">${player.rating}</span>
+                    <span class="player2-positon">${player.position}</span>
+                `;
+            }
+        }
+}
 
