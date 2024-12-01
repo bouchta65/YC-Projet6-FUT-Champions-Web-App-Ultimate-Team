6606b3d116252aecbc92fbbc3828e2a44058037c
bouchta65 <bouchtamohamed01@gmail.com>
2024-12-01 02:00:38 +0100

    refactor: move API consumption logic to home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,22 @@
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
+const api = "https://bouchta65.github.io/YC-Projet6-FUT-Champions-Web-App-Ultimate-Team/api/formatted_players.json";
+
+async function playersapi() {
+  const response = await fetch(api);
+  const data = await response.json();
+  localStorage.setItem("players", JSON.stringify(data.players));
+  
+}
+
+playersapi()
+
+
 
 burger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
   burger.classList.toggle('active'); 
 });
+
+
+//fetshing the data
